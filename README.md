6207e7db499565f6aca35b0ef60822969c64b401
Aventerine <aventerine@gmail.com>
2018-11-10 13:11:30 -0500

    Removed Mapbox Token and Updated README.md

--- README.md
+++ README.md
@@ -12,6 +12,8 @@ Udacity Front End Nanodegree program project.
 
 This repository uses [leafletjs](https://leafletjs.com/) with [Mapbox](https://www.mapbox.com/).
 
+You will need a Mapbox Token to make this work.
+
 ### Note about ES6
 
 Most of the code in this project has been written to the ES6 JavaScript specification for compatibility with modern web browsers and future proofing JavaScript code. As much as possible, try to maintain use of ES6 in any additional JavaScript you write. 
--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'pk.eyJ1IjoiY2F6emEiLCJhIjoiY2prOXU4NGpsMDJmZTN1cDgyaWgyZGNzaSJ9.-jDIg38xNNfKnWumNaEvGw',
+    mapboxToken: 'YOUR-MAPBOX-TOKEN',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
